b2f60d2d2fc66b25de6959c1517ecd5f12c06267
Neev Grover <groverneev@gmail.com>
2025-06-20 20:23:10 -0700

    idk_what_to_write

--- app/resources/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ maxWidth: 700, margin: '2rem auto', padding: 24 }}>
-      <h1>Resources</h1>
-      <p>Coming soon...</p>
-    </main>
-  )
-}
