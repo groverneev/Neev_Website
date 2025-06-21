61055ca08ce037be07ab08d32d5eeb5fc9886be0
Neev Grover <groverneev@gmail.com>
2025-06-20 20:36:31 -0700

    idk_what_to_write

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        background: '#FFFFFF',
+        background: '#0E1321',
         color: '#fff',
         textAlign: 'center',
         padding: '0 2rem'
