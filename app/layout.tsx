3438caaa5326cff338486096f9d6f574d13c0109
Neev Grover <groverneev@gmail.com>
2025-06-26 20:09:09 -0700

    idk_what_to_write

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+           <body style={{ margin: 0, padding: 0, minHeight: '100vh', boxSizing: 'border-box' }}>
+       <Navbar />
+  <main style={{ minHeight: '80vh' }}>
+       {children}
+    </main>
+    <Footer />
+     </body>
+
       <head>
         {/* ——— Font Awesome CDN ——— */}
         <link
@@ -38,7 +46,6 @@ export default function RootLayout({
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         />
       </head>
-      <body>{children}</body>
     </html>
   );
 }
