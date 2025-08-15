a17021b73f5f1e12ead3bc7ef9d6ea8190193afa
Neev Grover <groverneev@gmail.com>
2025-08-14 22:47:24 -0700

    fixed footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,64 @@
+import { FaGithub } from "react-icons/fa";
+import { SiSubstack } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 export default function Footer() {
   return (
     <footer
       style={{
         marginTop: 48,
         padding: 24,
-        background: "#f1f5f9",
+        background: "#0e1321",
         textAlign: "center",
-        color: "#64748b",
+        color: "#828893",
         fontSize: 14,
       }}
     >
-      &copy; {new Date().getFullYear()} Neev Grover. All rights reserved.
+      &copy; {new Date().getFullYear()} DuneBroom. All rights reserved.
       <br />
+      {/* Social Media Icons */}
+      <div style={{ marginTop: 16 }}>
+        {/* GitHub */}
+        <a
+          href="https://github.com/groverneev"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#828893", margin: "0 12px" }}
+        >
+          <FaGithub size={23} />
+        </a>
+
+        {/* Substack */}
+        <a
+          href="https://techunpacked.substack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#828893", margin: "0 12px" }}
+        >
+          <SiSubstack size={23} />
+        </a>
+
+        {/* X */}
+        <a
+          href="https://x.com/lightningpro535"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#828893", margin: "0 12px" }}
+        >
+          <FaXTwitter size={23} />
+        </a>
+
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/neev-grover-3bb873332/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#828893", margin: "0 12px" }}
+        >
+          <FaLinkedin size={23} />
+        </a>
+      </div>
     </footer>
   );
 }
