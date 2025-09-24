4e982d24c48dc51b5a39838833b6f7787c7e7a77
Neev Grover <groverneev@gmail.com>
2025-09-24 14:46:53 -0700

    finished 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,13 @@
 export default function NotFound() {
   return (
-    <main style={{ background: "#000", minHeight: "80vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+    <main style={{ background: "#0e1321", minHeight: "80vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
       <div style={{ color: "#fff", textAlign: "center" }}>
         <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: "16px" }}>
-          <h1 style={{ fontSize: "64px", fontWeight: "bold", margin: 0 }}>404</h1>
-          <div style={{ width: "1px", height: "64px", background: "#fff" }}></div>
-          <p style={{ fontSize: "20px", margin: 0 }}>This page could not be found.</p>
+          <h1 style={{ fontSize: "20px", fontWeight: "bold", margin: 0 }}>404</h1>
+          <div style={{ width: "1px", height: "64px", background: "#555555" }}></div>
+          <p style={{ fontSize: "20px", margin: 0 }}>This page could not be found</p>
         </div>
       </div>
     </main>
   );
 }
-
-
